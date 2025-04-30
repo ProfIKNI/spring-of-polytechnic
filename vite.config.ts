fc75215338ab4a490501245c41141f779fb3e33b
p-i-r-u-m <igr.case@gmail.com>
2025-04-30 19:35:11 +0300

    feat(pre-commit): test husky #1

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/spring-of-polytechnic/",
+  base: "/spring-of-polytechnic/", // важливо для деплою!
 })
+
