c440c20cfa387cc5dbb3f927316fcd1211447c01
Andrii <andriikhavalko@gmail.com>
2025-05-01 09:25:59 +0300

    added primary page

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+// tailwind.config.js
+module.exports = {
+    theme: {
+      extend: {
+        animation: {
+          move: 'move 3s ease-in-out infinite',
+        },
+        keyframes: {
+          move: {
+            '0%': { transform: 'translateX(0)' },
+            '50%': { transform: 'translateX(50px)' },
+            '100%': { transform: 'translateX(0)' },
+          },
+        },
+      },
+    },
+    plugins: [],
+  }
+  
