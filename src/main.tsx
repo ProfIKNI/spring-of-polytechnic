9bafff174f90977706249a43d626467e45f632a2
Andrii <andriikhavalko@gmail.com>
2025-05-02 21:54:29 +0300

    added new buy ticket page and added router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import "./global.css";
 import App from "./App.tsx";
+import BuyTicketPage from "./pages/BuyTicketPage.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: '/spring-of-polytechnic/',
+    element: <App />,
+  },
+  {
+    path: '/spring-of-polytechnic/buy-ticket',
+    element: <BuyTicketPage />,
+  },
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 );
+
