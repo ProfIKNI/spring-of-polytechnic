9ef160eecd45024959406630be931034dcc16d78
Andrew Kozovyi <andriy.kozovyi@gmail.com>
2025-05-05 23:24:50 +0300

    shadow

--- src/pages/MainMenuNavButton.tsx
+++ src/pages/MainMenuNavButton.tsx
@@ -5,7 +5,7 @@ const MainMenuNavButton = ({ title, icon, route } : DetailedIcon) => {
 
     return (
         <Link to={route}
-            className={`${window.innerWidth < 390 ? "w-28 h-28" : "w-32 h-32"} p-4 rounded-[30%] bg-white/70 font-playFont flex flex-col items-center justify-end`}>
+            className={`${window.innerWidth < 390 ? "w-28 h-28" : "w-32 h-32"} p-4 rounded-[30%] bg-white/70 shadow-[0px_4px_8.7px_5px_rgba(255,255,255,0.25)] font-playFont flex flex-col items-center justify-end `}>
             <img src={icon} alt={title}/>
             <h2 className="text-[#98835E] drop-shadow-[4px_4px_4px_rgba(0,0,0,0.4)]">{title}</h2>
         </Link>
