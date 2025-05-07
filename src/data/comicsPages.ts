773313a0cba8fca62ce2a5f5437c0aea46b54b04
Andrew Kozovyi <andriy.kozovyi@gmail.com>
2025-05-07 20:17:15 +0300

    pc as well

--- src/data/comicsPages.ts
+++ src/data/comicsPages.ts
@@ -9,6 +9,17 @@ import PhonePage3Part1 from "../assets/comics_phone_page_3_part_01.png";
 import PhonePage3Part2 from "../assets/comics_phone_page_3_part_02.png";
 import PhonePage3Part3 from "../assets/comics_phone_page_3_part_03.png";
 import PhonePage3Part4 from "../assets/comics_phone_page_3_part_04.png";
+import PcPage1Part1 from "../assets/comics_pc_page_1_part_01.png";
+import PcPage1Part2 from "../assets/comics_pc_page_1_part_02.png";
+import PcPage1Part3 from "../assets/comics_pc_page_1_part_03.png";
+import PcPage1Part4 from "../assets/comics_pc_page_1_part_04.png";
+import PcPage1Part5 from "../assets/comics_pc_page_1_part_05.png";
+import PcPage2Part1 from "../assets/comics_pc_page_2_part_01.png";
+import PcPage2Part2 from "../assets/comics_pc_page_2_part_02.png";
+import PcPage3Part1 from "../assets/comics_pc_page_3_part_01.png";
+import PcPage3Part2 from "../assets/comics_pc_page_3_part_02.png";
+import PcPage3Part3 from "../assets/comics_pc_page_3_part_03.png";
+import PcPage3Part4 from "../assets/comics_pc_page_3_part_04.png";
 
 export interface ComicsPage {
     img: string;
@@ -54,5 +65,46 @@ export const PhoneComicsPages: ComicsPage[] =[
     },
     {
         img: PhonePage3Part4,
+    }
+]
+
+export const PcComicsPages: ComicsPage[] =[
+    {
+        img: PcPage1Part1,
+        cursor: "top-3/5 left-1/5"
+    },
+    {
+        img: PcPage1Part2,
+        cursor: "top-1/2 right-1/2"
+    },
+    {
+        img: PcPage1Part3,
+        cursor: "top-1/5 right-1/5"
+    },
+    {
+        img: PcPage1Part4,
+        cursor: "top-3/5 right-1/5"
+    },
+    {
+        img: PcPage1Part5
+    },
+    {
+        img: PcPage2Part1,
+        animated: PcPage2Part2
+    },
+    {
+        img: PcPage3Part1,
+        cursor: "top-3/5 left-1/5"
+    },
+    {
+        img: PcPage3Part2,
+        cursor: "top-1/5 right-1/5"
+    },
+    {
+        img: PcPage3Part3,
+        cursor: "top-3/5 right-1/5"
+    },
+    {
+        img: PcPage3Part4,
     }
 ]
